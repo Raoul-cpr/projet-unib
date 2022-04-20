8141e7833bdd5126a06bc0dd96095eb52a993330
raoul compaore <raoul2compaore@gmail.com>
2022-04-20 18:20:44 +0000

    Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:unib/page.dart';
 import 'package:unib/textfield.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyBw-7EeEj6sTz-yWTmnJGNp0um1bwPdKWk",
+            projectId: "projet-unib",
+            storageBucket: "projet-unib.appspot.com",
+            messagingSenderId: "301305619011",
+            appId: "1:301305619011:web:057358961a63646c1aeaf6"));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
